feb81ada202a7e3413d06982aeb573d39eec34ad
张小雅 <atomgoto@gmail.com>
2026-01-28 19:13:04 +0800

    fix bug: nested grid ww3_ounp fail

--- public/ww3/local.sh
+++ public/ww3/local.sh
@@ -83,6 +83,7 @@ if [ -d "coarse" ] && [ -d "fine" ]; then
     
     [ -f out_grd.fine ] && mv out_grd.fine fine/out_grd.ww3
     [ -f mod_def.fine ] && mv mod_def.fine fine/mod_def.ww3
+    [ -f out_pnt.fine ] && mv out_pnt.fine fine/out_pnt.ww3
     
     cd fine
     if [ -f points.list ]; then
